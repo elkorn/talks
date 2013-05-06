ac85c5956f484aa681f9270695168ea3201cccac
elkorn <helluinster@gmail.com>
2013-05-06 19:41:29 +0200

    Updating presentation.

--- presentation/app.js
+++ presentation/app.js
@@ -1,5 +1,10 @@
-(function(){
+(function() {
     var counter = new Counter();
 
-    
+    function updateSlideCount() {
+        document.getElementById('count').innerHTML = [counter.getCurrent(), '/', counter.getTotal()].join();
+    }
+
+    window.onhashchange = updateSlideCount;
+    updateSlideCount();
 }());
