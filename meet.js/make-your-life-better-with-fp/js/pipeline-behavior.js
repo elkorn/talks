f50a3bcf302d8308cf3efd856a88539d6f53ad98
elkorn <helluinster@gmail.com>
2013-12-01 20:02:05 +0100

    [Make your life better with FP@meet.js] minor fixes

--- meet.js/make-your-life-better-with-fp/js/pipeline-behavior.js
+++ meet.js/make-your-life-better-with-fp/js/pipeline-behavior.js
@@ -1,16 +1,28 @@
 /* module 4, behavior orchestration */
-var publicPipeline = {
+
+function PipelineBuilder(options) {
+    this.before = options.before;
+    this.after = options.after;
+}
+
+PipelineBuilder.prototype = {
+    buildPipeline: function(action) {
+        // Build the pipeline and then wrap each step with a maybe just in case something goes wrong.
+        // The actions have to be reversed due to RTL compoosition exec order.
+        return mapWith(maybe)(this.before.concat(action).concat(this.after).reverse());
+    }
+};
+
+var publicPipeline = new PipelineBuilder({
     before: [validateData, logState],
     after: [informAboutResults, logState, saveToDatabase, notifyBusinessPeople]
-};
+});
 
-var skimmerPipeline = { 
+var skimmerPipeline = new PipelineBuilder({ 
     before: [hijackCustomerInfo, validateData, logState],
     after: [informAboutResults, skimSomeDollars, logState, saveToDatabase, notifyBusinessPeople]
-};
+});
 
-function includeInAspectPipeline(pipeline, action) { 
-    // Build the pipeline and then wrap each step with a maybe just in case something goes wrong.
-    var readyPipeline = mapWith(maybe)(pipeline.before.concat(action).concat(pipeline.after).reverse());
-    return _.compose.apply(_, readyPipeline);
+function includeInAspectPipeline(pipelineBuilder, action) { 
+    return _.compose.apply(_, pipelineBuilder.buildPipeline(action));
 }
